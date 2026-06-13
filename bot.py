2b750f5f2c634aa6688f7babdc1bd5d357386535
tibrunoplay-star <tibrunoplay@gmail.com>
2026-06-13 19:33:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,21 @@
 import os
 import discord
 from discord.ext import commands
+import psycopg
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+conn = psycopg.connect(DATABASE_URL)
+
+with conn.cursor() as cur:
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS ranking_semanal (
+            motorista TEXT PRIMARY KEY,
+            km BIGINT NOT NULL DEFAULT 0
+        )
+    """)
+    conn.commit()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,5 +25,10 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Bot ligado: {bot.user}")
+    print("Base de dados ligada com sucesso!")
+
+@bot.command()
+async def ranking(ctx):
+    await ctx.send("Base de dados OK ✅")
 
 bot.run(TOKEN)
