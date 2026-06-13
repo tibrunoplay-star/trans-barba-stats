c56f34b89a97ad03798ae12a7b9b62ae84b62374
tibrunoplay-star <tibrunoplay@gmail.com>
2026-06-13 16:09:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,10 @@ import discord
 from discord.ext import commands
 import os
 
+import os
+
 TOKEN = os.getenv("DISCORD_TOKEN")
+print("TOKEN =", TOKEN)
 
 intents = discord.Intents.default()
 intents.message_content = True
