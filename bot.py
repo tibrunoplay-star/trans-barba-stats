aaef73e3545c4e317dc85c621671726fee40575b
tibrunoplay-star <tibrunoplay@gmail.com>
2026-06-13 19:08:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,107 +1,16 @@
+import os
 import discord
 from discord.ext import commands
-import psycopg
-import os
-import re
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Liga à base de dados
-conn = psycopg.connect(DATABASE_URL)
-
-# Cria tabela
-with conn.cursor() as cur:
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS ranking_semanal (
-            motorista TEXT PRIMARY KEY,
-            km BIGINT NOT NULL DEFAULT 0
-        )
-    """)
-    conn.commit()
-
-
 @bot.event
 async def on_ready():
     print(f"Bot ligado: {bot.user}")
-
-
-@bot.event
-async def on_message(message):
-
-    if message.author.bot and message.embeds:
-
-        try:
-            embed = message.embeds[0]
-
-            motorista = embed.author.name
-
-            detalhes = None
-
-            for field in embed.fields:
-                if field.name == "Detalhes":
-                    detalhes = field.value
-                    break
-
-            if detalhes:
-
-                match = re.search(
-                    r"Distância Aceita:\s*([\d\s]+)\s*km",
-                    detalhes
-                )
-
-                if match:
-
-                    km = int(match.group(1).replace(" ", ""))
-
-                    with conn.cursor() as cur:
-                        cur.execute("""
-                            INSERT INTO ranking_semanal (motorista, km)
-                            VALUES (%s, %s)
-                            ON CONFLICT (motorista)
-                            DO UPDATE SET
-                            km = ranking_semanal.km + EXCLUDED.km
-                        """, (motorista, km))
-
-                        conn.commit()
-
-                    print(f"{motorista} +{km} km")
-
-        except Exception as e:
-            print("ERRO:", e)
-
-    await bot.process_commands(message)
-
-
-@bot.command()
-async def ranking(ctx):
-
-    with conn.cursor() as cur:
-
-        cur.execute("""
-            SELECT motorista, km
-            FROM ranking_semanal
-            ORDER BY km DESC
-            LIMIT 10
-        """)
-
-        rows = cur.fetchall()
-
-    if not rows:
-        await ctx.send("Sem dados.")
-        return
-
-    texto = "🏆 Ranking Semanal\n\n"
-
-    for pos, (nome, km) in enumerate(rows, start=1):
-        texto += f"{pos}. {km:,} km - {nome}\n"
-
-    await ctx.send(f"```{texto}```")
-
 
 bot.run(TOKEN)
