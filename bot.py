1d96882ae4d909c87c4e6add46f31a9f3ad516a7
tibrunoplay-star <tibrunoplay@gmail.com>
2026-06-13 17:19:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 import re
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = ${{Postgres.DATABASE_URL}}
 
 intents = discord.Intents.default()
 intents.message_content = True
