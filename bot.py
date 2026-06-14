04fe63f142f874dbd2a19993e31db4aae0e8d151
tibrunoplay-star <tibrunoplay@gmail.com>
2026-06-14 02:40:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -166,7 +166,7 @@ async def ranking(ctx):
     await ctx.send(f"```{texto}```")
 
 CANAL_LIDER_ID = 1515340410694664344
-MENSAGEM_LIDER_ID = 1515518894813483059
+MENSAGEM_LIDER_ID = 1515530962837045370
 
 async def atualizar_lider():
 
